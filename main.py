d873a2e26d2fbec63e01ba00159915bb0bd9ae45
Sandoval <csandoval27@cps.k12.il.us>
2023-10-04 09:00:49 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# data abstraction is when you need to simplify data from something more complex
+# an exmaple of data abstraction is when I use a list to simplify data instead of having
+# to writw out the same code over and over again
+# data abstraction is when you simplify data - if you have a lists of names, you can
+# simplify it by using a list
